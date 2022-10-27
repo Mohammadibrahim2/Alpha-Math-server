1116dd780d17cebefc9be5f7aa3df632d4924bf0
Mohammad ibrahim <mohammadibrahim6454@gmail.com>
2022-10-27 10:52:20 +0600

    after update the course details page by addding some list

--- src/Pages/Courses/CourseDetails/CourseDetails.js
+++ src/Pages/Courses/CourseDetails/CourseDetails.js
@@ -2,28 +2,44 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const CourseDetails=()=>{
 
     const courseDetails=useLoaderData()
-    const{subtittle,img,details,tittle}=courseDetails
+    const{subtittle,img,details,tittle,subdetals,catagories}=courseDetails
     console.log(courseDetails)
 return(
-    <div>
+    <Container>
+      
     <Card style={{ width: '70%',margin:"0 auto" }}>
       <Card.Img className="h-lg-50 h-100" src={img} />
       <Card.Header><h2>{tittle}</h2></Card.Header>
+      <Card.Title><h5>{subtittle}</h5></Card.Title>
       <Card.Body>
-        <Card.Title><h4>{subtittle}</h4></Card.Title>
+      <Row>
+        <Col lg="3">
+          {
+            catagories.map(catagory=><li>{catagory}</li>)
+          }
+
+
+        </Col>
+        <Col lg="9">
         <Card.Text>
          <p>{details}</p>
+         <p>{subdetals}</p>
         </Card.Text>
+        </Col>
+        </Row>
         <Button >more information</Button>
       </Card.Body>
     </Card>
 
-
-    </div>
+  
+    </Container>
 )
 
 
